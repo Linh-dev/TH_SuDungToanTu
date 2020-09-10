c4788cb36958851304703df5b67658cb52274548
Linh-dev <70752357+Linh-dev@users.noreply.github.com>
2020-09-10 10:20:00 +0700

    add all file

---
+++ js/js1.js
@@ -0,0 +1,10 @@
+let Dai = prompt('Nhap Chieu Dai:');
+let Rong = prompt('Nhap Chieu Rong:');
+let typeRong = typeof Rong;
+let typeDai = typeof Dai;
+let s = Rong*Dai
+/*document.write(typeDai)
+document.write('<br>')
+document.write(typeRong)
+document.write('<br>')*/
+document.write('S hinh chu nhat co chieu dai = '+Dai+' va chieu Rong = '+Rong+' la: '+s)
